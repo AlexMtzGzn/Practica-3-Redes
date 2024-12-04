e01120ca0853682910f6f1167b96e799bd5d7350
AlexMtzGzn <alex13141926@gmail.com>
2024-12-03 18:34:15 -0600

    ajuste en ñas redes y ajustes en el eth

--- shared/labarp/eth.h
+++ shared/labarp/eth.h
@@ -11,28 +11,30 @@
 #include <netinet/ether.h>
 #include <unistd.h>
 
+/*Longitud del payload*/
+#define ETHER_TYPE 60
+/*Con 2000 bytes son suficientes para la trama, ya que va de 64 a 1518*/
+#define BUF_SIZ 2000
 
- /*Longitud del payload*/ 
-#define ETHER_TYPE   60
- /*Con 2000 bytes son suficientes para la trama, ya que va de 64 a 1518*/
-#define BUF_SIZ      2000    
-
- /*Tipo de dato sin signo*/
+/*Tipo de dato sin signo*/
 typedef unsigned char byte;
 
+void ConvierteMAC(char *Mac, char *Org)
+{ /*En lugar de caracteres, se requiere el numero*/
+  int i, j, Aux, Acu;
+  for (i = 0, j = 0, Acu = 0; i < 12; i++)
+  {
+    if ((Org[i] > 47) && (Org[i] < 58))
+      Aux = Org[i] - 48;
+    if ((Org[i] > 64) && (Org[i] < 97))
+      Aux = Org[i] - 55;
 
- void ConvierteMAC (char *Mac, char *Org)
- { /*En lugar de caracteres, se requiere el numero*/
-   int i, j, Aux, Acu;
-   for (i=0, j=0, Acu=0; i<12; i++)
-   {
-     if ((Org[i]>47)&&(Org[i]<58)) Aux = Org[i] - 48;
-     if ((Org[i]>64)&&(Org[i]<97)) Aux = Org[i] - 55;
-     if (Org[i]>96) Aux = Org[i] - 87;     
-     if ((i%2)==0) Acu = Aux * 16;
-     else 
-     {
-       Mac[j] = Acu + Aux;  j++;
-     }
-   }
- }
+    if (Org[i] > 96)
+      Aux = Org[i] - 87;
+    if ((i % 2) == 0)
+      Acu = Aux * 16;
+    else
+      Mac[j] = Acu + Aux;
+    j++;
+  }
+}
--- sw1.sh 
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Configurar bridge
-ip link add name br0 type bridge
-ip link set dev eth0 master br0
-ip link set dev eth1 master br0
-ip link set dev eth2 master br0
-ip link set dev eth3 master br0
-ip link set dev eth4 master br0
-ip link set dev br0 up
